f8cf1eebfae2f377905e2976e54a50907162d74d
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2025-01-21 15:22:47 +0200

    feat/created diary module, component and routing

---
+++ src/app/diary/diary.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiaryComponent } from './diary/diary.component';
+import { DiaryRoutingModule } from './diary-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    DiaryComponent
+  ],
+  imports: [
+    CommonModule, DiaryRoutingModule
+  ]
+})
+export class DiaryModule { }
