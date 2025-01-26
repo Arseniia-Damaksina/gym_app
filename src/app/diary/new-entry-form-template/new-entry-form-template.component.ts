450e231f9d3a5389a87630a50fe0ac7a0ecd6e49
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2025-01-26 19:39:46 +0200

    fixed the mistake in the TD form

--- src/app/diary/new-entry-form-template/new-entry-form-template.component.ts
+++ src/app/diary/new-entry-form-template/new-entry-form-template.component.ts
@@ -1,26 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { ExerciseSet } from '../interfaces/exercise-set';
 import { ExerciseSetsService } from '../services/exercise-sets.service';
 import { Router } from '@angular/router';
-import { ExerciseSet } from '../interfaces/exercise-set';
 
 @Component({
   selector: 'app-new-entry-form-template',
   templateUrl: './new-entry-form-template.component.html',
-  styleUrl: './new-entry-form-template.component.css',
+  styleUrls: ['./new-entry-form-template.component.css'],
 })
 export class NewEntryFormTemplateComponent {
   private exerciseSetsService = inject(ExerciseSetsService);
   private router = inject(Router);
-  entry: ExerciseSet = {
-    date: new Date(),
-    exercise: '',
-    sets: 0,
-    reps: 0,
-  };
+
+  entry: ExerciseSet = { date: new Date(), exercise: '', reps: 0, sets: 0 };
 
   newEntry() {
     const newEntry = { ...this.entry };
-
+    
     this.exerciseSetsService
       .addNewItem(newEntry)
       .subscribe((entry) => this.router.navigate(['/home']));
