5a5feb0002988231df122726834328cb2274342b
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2025-02-16 19:43:07 +0100

    added test file for the diary resolver

---
+++ src/app/diary/diary.resolver.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { ResolveFn } from '@angular/router';
+
+import { diaryResolver } from './diary.resolver';
+import { ExerciseSetList } from './interfaces/exercise-set';
+
+describe('diaryResolver', () => {
+  const executeResolver: ResolveFn<ExerciseSetList> = (...resolverParameters) => 
+      TestBed.runInInjectionContext(() => diaryResolver(...resolverParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeResolver).toBeTruthy();
+  });
+});
+// git config --global user.name "Arseniia-Damaksina"
