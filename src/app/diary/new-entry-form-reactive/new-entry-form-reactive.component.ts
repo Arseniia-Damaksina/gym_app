f75c5695313c5ef43721ccf840e59984c027dcc0
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2025-02-16 12:55:16 +0200

    added filter through suggestions of exercises

--- src/app/diary/new-entry-form-reactive/new-entry-form-reactive.component.ts
+++ src/app/diary/new-entry-form-reactive/new-entry-form-reactive.component.ts
@@ -11,6 +11,15 @@ import { multipleValidator } from './custom-validation';
 import { ExerciseSet } from '../interfaces/exercise-set';
 import { formatDate } from '../../utils/formatDate';
 import { ExercisesService } from '../services/exercises.service';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs';
+
+const DEBOUNCE_TIME = 300;
 
 @Component({
   selector: 'app-new-entry-form-reactive',
@@ -19,36 +28,35 @@ import { ExercisesService } from '../services/exercises.service';
 })
 export class NewEntryFormReactiveComponent implements OnInit {
   @Input('id') entryId?: string;
-  public entryForm!: FormGroup;
   private formBuilder = inject(NonNullableFormBuilder);
   private exerciseSetsService = inject(ExerciseSetsService);
   private exerciseService = inject(ExercisesService);
   public showSuggestions: boolean = false;
-  public exercises$ = this.exerciseService.getExercises();
+  // public exercises$ = this.exerciseService.getExercises();
   private router = inject(Router);
   private route = inject(ActivatedRoute);
+  public entryForm = this.formBuilder.group({
+    date: [new Date(), Validators.required],
+    exercise: ['', Validators.required],
+    sets: [0, [Validators.required, Validators.min(0), multipleValidator(2)]],
+    reps: [0, [Validators.required, Validators.min(0), multipleValidator(3)]],
+  });
+
+  public exercises$ = this.entryForm.valueChanges.pipe(
+    debounceTime(DEBOUNCE_TIME),
+    map((model) => model?.exercise ?? ''),
+    filter((exercise) => exercise.length >= 3),
+    distinctUntilChanged(),
+    switchMap((exercise) => this.exerciseService.getExercises(exercise))
+  );
 
   ngOnInit(): void {
     // this.entryId = this.route.snapshot.paramMap.get('id');
-    this.entryForm = this.formBuilder.group({
-      date: [new Date(), Validators.required],
-      exercise: ['', Validators.required],
-      sets: [
-        '',
-        [Validators.required, Validators.min(0), multipleValidator(2)],
-      ],
-      reps: [
-        '',
-        [Validators.required, Validators.min(0), multipleValidator(3)],
-      ],
-    });
-
     if (this.entryId) {
-      this.route.data.subscribe(({entry}) => {
+      this.route.data.subscribe(({ entry }) => {
         this.updateForm(entry);
-      })
+      });
     }
-
   }
 
   selectExercise(suggestion: string) {
@@ -62,11 +70,7 @@ export class NewEntryFormReactiveComponent implements OnInit {
 
   updateForm(entry: ExerciseSet): void {
     let { id: _, ...entryForm } = entry;
-    const formattedEntry = {
-      ...entryForm,
-      date: formatDate(entry.date),
-    };
-    this.entryForm.setValue(formattedEntry);
+    this.entryForm.setValue(entryForm);
   }
 
   newEntry() {
